5ca8b9ca3e6dc352fb487b543b435eb4e3c11f3e
KehindeHussein <kehindehussein01@gmail.com>
2019-01-05 22:12:54 +0100

    Done with Project

--- app/src/main/java/com/badrudeen/myself/Display.java
+++ app/src/main/java/com/badrudeen/myself/Display.java
@@ -1,15 +1,21 @@
 package com.badrudeen.myself;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
+import static com.badrudeen.myself.MainActivity.Email;
+import static com.badrudeen.myself.MainActivity.Message;
 import static com.badrudeen.myself.MainActivity.Name1;
+import static com.badrudeen.myself.MainActivity.Phone;
 
 public class Display extends AppCompatActivity {
+
+    TextView name, phone, email, message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,37 @@ public class Display extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Name1 = "";
+        name = findViewById(R.id.displayname);
+        phone = findViewById(R.id.displaynumber);
+        email = findViewById(R.id.displayemail);
+        message = findViewById(R.id.displaymessage);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        name.setText(Name1);
+        phone.setText(Phone);
+        email.setText(Email);
+        message.setText(Message);
+
+
+        findViewById(R.id.call).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent call = new Intent(Intent.ACTION_DIAL);
+                call.setData(Uri.parse("tel:" + Phone));
+                if (call.resolveActivity(getPackageManager()) != null){
+                    startActivity(call);
+                }
+            }
+        });
+
+        findViewById(R.id.sentemail).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent email = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" +Email));
+                email.putExtra(Intent.EXTRA_SUBJECT, "Message From MyFreind App");
+                email.putExtra(Intent.EXTRA_TEXT, Message);
+                if (email.resolveActivity(getPackageManager()) != null){
+                    startActivity(Intent.createChooser(email,"Send email..."));
+                }
             }
         });
     }
